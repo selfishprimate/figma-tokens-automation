3248c9b25a0351b74078882195b35b2433a51402
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2022-12-15 22:01:42 +0300

    Add config.js file.

---
+++ config.js
@@ -0,0 +1,23 @@
+const StyleDictionary = require('style-dictionary');
+
+module.exports = {
+  source: ['tokens/**/*.json'],
+  platforms: {
+    "scss": {
+      "transformGroup": "scss",
+      "buildPath": "build/scss/",
+      "files": [{
+        "destination": "_variables.scss",
+        "format": "scss/variables"
+      }]
+    },
+    "css": {
+      "transformGroup": "css",
+      "buildPath": "build/css/",
+      "files": [{
+        "destination": "variables.css",
+        "format": "css/variables"
+      }]
+    }
+  }
+}
