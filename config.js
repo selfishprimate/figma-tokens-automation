a1cdbb65aabe612b10d0415c3293cd95b9c7b1eb
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2022-12-16 17:26:05 +0300

    Typography expanded.

--- config.js
+++ config.js
@@ -22,4 +22,16 @@ module.exports = {
   }
 }
 
-console.log('Build completed!');
+/**
+ * Transform typography shorthands for css variables
+ */
+StyleDictionary.registerTransform({
+  name: "typography/shorthand",
+  type: "value",
+  transitive: true,
+  matcher: (token) => token.type === "typography",
+  transformer: (token) => {
+    const {fontWeight, fontSize, lineHeight, fontFamily} = token.original.value;
+    return `${fontWeight} ${fontSize}/${lineHeight} ${fontFamily}`;
+  },
+});
