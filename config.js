a3ea5ac66383b183616d6f3951cc9fe317f135cf
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2022-12-31 22:53:09 +0300

    Add transform for JavaScript.

--- config.js
+++ config.js
@@ -18,20 +18,14 @@ module.exports = {
         "destination": "variables.css",
         "format": "css/variables"
       }]
+    },
+    "js": {
+      "transformGroup": "js",
+      "buildPath": "build/js/",
+      "files": [{
+        "destination": "variables.js",
+        "format": "javascript/es6"
+      }]
     }
   }
 }
-
-/**
- * Transform typography shorthands for css variables
- */
-StyleDictionary.registerTransform({
-  name: "typography/shorthand",
-  type: "value",
-  transitive: true,
-  matcher: (token) => token.type === "typography",
-  transformer: (token) => {
-    const {fontWeight, fontSize, lineHeight, fontFamily} = token.original.value;
-    return `${fontWeight} ${fontSize}/${lineHeight} ${fontFamily}`;
-  },
-});
